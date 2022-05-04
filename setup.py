4650a8d3588137ea5716fd7f9e362d8f76db9683
NINELIE <100738397+BugeonBG@users.noreply.github.com>
2022-05-04 09:48:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ setuptools.setup (
   author='BugeonBG',
   author_email='jbg0118@naver.com',
   url = "https://github.com/BugeonBG/mycalculator",
-  download_url = " 
+  download_url = "https://github.com/BugeonBG/mycalculator/releases/tag/v0.0.1",
+   install_requires=['pytest']
+   long_description = 'oss-dev calculator python module',
+    long_description_content_type = 'text/markdown',
+    classifiers=[
+        "programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
