7a5a9d9808cef69aecbb7ab2615b3102cd37f55c
NINELIE <100738397+BugeonBG@users.noreply.github.com>
2022-05-04 09:42:40 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup (
+    include_package_data=true,
+  name='calculator',
+  version='0.0.1',
+  description='oss-dev my calculator',
+  author='BugeonBG',
+  author_email='jbg0118@naver.com',
+  url = "https://github.com/BugeonBG/mycalculator",
+  download_url = " 
